3f9a37aef40f58907d001b1412152cf38255621c
Kotsuu <princecooler101@gmail.com>
2025-02-19 22:56:36 +0800

    CHANGES FINALIZED

---
+++ LinkedList/SinglyLinkedList/SinglyLinkedList.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class SinglyLinkedList {
+private:
+    Node* head;
+
+public:
+    SinglyLinkedList() {
+        head = nullptr;
+    }
+
+    void insert(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void remove(int value) {
+        if (head == nullptr) {
+            cout << "List is empty.\n";
+            return;
+        }
+        if (head->data == value) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next != nullptr && temp->next->data != value) {
+            temp = temp->next;
+        }
+        if (temp->next == nullptr) {
+            cout << "Value not found in the list.\n";
+            return;
+        }
+        Node* nodeToDelete = temp->next;
+        temp->next = temp->next->next;
+        delete nodeToDelete;
+    }
+
+    bool search(int value) {
+        Node* temp = head;
+        while (temp != nullptr) {
+            if (temp->data == value)
+                return true;
+            temp = temp->next;
+        }
+        return false;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+
+    ~SinglyLinkedList() {
+        Node* temp;
+        while (head != nullptr) {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    SinglyLinkedList list;
+    list.insert(10);
+    list.insert(20);
+    list.insert(30);
+    list.insert(40);
+
+    cout << "Linked List: ";
+    list.display();
+
+    cout << "Searching for 20: " << (list.search(20) ? "Found" : "Not Found") << "\n";
+    cout << "Deleting 20...\n";
+    list.remove(20);
+
+    cout << "Linked List after deletion: ";
+    list.display();
+
+    return 0;
+}
